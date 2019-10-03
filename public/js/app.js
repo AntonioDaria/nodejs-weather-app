f83ffcb1ab8899cc602bfe91356ce42427c897cf
antonio <antoniodaria@msn.com>
2019-10-03 17:30:16 +0100

    set up to deploy to heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e) => {
         e.preventDefault()
         const location = search.value
-        const baseUrl = "http://localhost:3000/weather?address=" + location
+        const baseUrl = "/weather?address=" + location
         messageOne.textContent = 'Loading...'
         messageTwo.textContent = ""
 
